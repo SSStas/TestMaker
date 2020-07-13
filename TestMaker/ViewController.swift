031742574135c2e004fe02dd37d964db648ae5f2
Stanislav Nikitin <stas.niki@mail.ru>
2020-07-13 14:42:24 +0300

    Initial Commit

---
+++ TestMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMaker
+//
+//  Created by Mac on 13.07.2020.
+//  Copyright © 2020 Apples. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
